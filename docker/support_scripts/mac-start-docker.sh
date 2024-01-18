6ceb43951ef1107a05e6f4bd4eb5fb89d44abb38
Yoichiro Hasebe <yohasebe@gmail.com>
2024-01-18 09:41:05 +0900

    monadic.sh modified to improve build/start/stop processes

--- docker/support_scripts/mac-start-docker.sh
+++ docker/support_scripts/mac-start-docker.sh
@@ -3,10 +3,10 @@
 # Start Docker Desktop
 open -a Docker
 
+sleep 5
+
 # Wait for Docker to become available
 until /usr/local/bin/docker info > /dev/null 2>&1; do
     echo "Waiting for Docker Desktop to start..."
     sleep 5
 done
-
-echo "Docker Desktop has been loaded"

5f0da98ba6cd5ba5d5ee696a0d311d33e00d3105
yohasebe <yohasebe@gmail.com>
2024-01-19 13:45:26 +0900

    monadic.sh and helper scripts modified

--- docker/support_scripts/mac-start-docker.sh
+++ docker/support_scripts/mac-start-docker.sh
@@ -6,7 +6,8 @@ open -a Docker
 sleep 5
 
 # Wait for Docker to become available
-until /usr/local/bin/docker info > /dev/null 2>&1; do
+# until /usr/local/bin/docker info > /dev/null 2>&1; do
+until docker info > /dev/null 2>&1; do
     echo "Waiting for Docker Desktop to start..."
     sleep 5
 done

b833837fa11edf1f42ec25e5fe5132eff3751d38
Yoichiro Hasebe <yohasebe@gmail.com>
2024-02-25 12:01:14 +0900

    retstructured the repo

--- server/apps/diagram_draft/diagram_draft_app.rb
+++ server/apps/diagram_draft/diagram_draft_app.rb
@@ -56,7 +56,7 @@ class DiagramDraft < MonadicApp
   def settings
     {
       "model": "gpt-3.5-turbo-0125",
-      "temperature": 0.5,
+      "temperature": 0.0,
       "top_p": 0.0,
       "max_tokens": 2000,
       "context_size": 10,
--- server/lib/monadic/version.rb
+++ server/lib/monadic/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Monadic
-  VERSION = "0.4.6"
+  VERSION = "0.4.7"
 end

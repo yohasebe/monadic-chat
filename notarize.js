4aa8bf36badedc0e1e0ef7449f39ab1a44a7745d
Yoichiro Hasebe <yohasebe@gmail.com>
2024-03-05 12:06:00 +0900

    node modules updated

--- notarize.js
+++ notarize.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
     const { electronPlatformName, appOutDir } = context;   
@@ -14,6 +14,7 @@ exports.default = async function notarizing(context) {
         appPath: `${appOutDir}/${appName}.app`,
         appleId: process.env.APPLEID,
         appleIdPassword: process.env.APPLEIDPASS,
-        ascProvider: process.env.ASCPROVIDER
+        ascProvider: process.env.ASCPROVIDER,
+        teamId: process.env.TEAMID
     });
 };

454c05b8c35adaa90086dc051b28357d2909d065
Yoichiro Hasebe <yohasebe@gmail.com>
2024-06-13 15:47:50 +0900

    fixed typos in base apps

--- docker/services/ruby/apps/chat/chat_app.rb
+++ docker/services/ruby/apps/chat/chat_app.rb
@@ -10,19 +10,19 @@ class Chat < MonadicApp
   def initial_prompt
     text = <<~TEXT
       You are a friendly and professional consultant with real-time, up-to-date information about almost anything. You are able to answer various types of questions, write computer program code, make decent suggestions, and give helpful advice in response to a prompt from the user. If the prompt is not clear enough, ask the user to rephrase it. Use the same language as the user and insert an emoji that you deem appropriate for the user's input at the beginning of your response.
+      
     TEXT
     text.strip
   end
 
   def ai_user_initial_prompt
     text = <<~TEXT
-      The user is currently answering various types of questions, writing computer program code, making decent suggestions, and giving helpful advice upon your message. Give the user requests, suggestions, or questions so that the conversation is engaging and interesting. If there are any errors in the responses you get, point them out and ask for correction. Use the same language as the user.
+      The user is currently answering various types of questions, writing computer program code, making decent suggestions, and giving helpful advice on your message. Give the user requests, suggestions, or questions so that the conversation is engaging and interesting. If there are any errors in the responses you get, point them out and ask for correction. Use the same language as the user.
 
-Keep on pretending as if you were the "user" and as if the user where the "assistant" throughout the conversation.
+Keep on pretending as if you were the "user" and as if the user were the "assistant" throughout the conversation.
 
-Do you best to make the flow of the conversation as natural as possible. Do not change subjects abruptly, and keep the conversation going by asking questions or making comments that are relevant to the preceding and current topics.
-    TEXT
-    text.strip
+Do your best to make the conversation as natural as possible. Do not change subjects abruptly, and keep the conversation going by asking questions or making comments relevant to the preceding and current topics. 
+  TEXT
   end
 
   def settings

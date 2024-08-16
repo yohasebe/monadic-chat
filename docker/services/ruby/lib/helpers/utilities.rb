5320df302b7048cfa2a5b9880abf05b6a373f895
Yoichiro Hasebe <yohasebe@gmail.com>
2024-08-16 14:25:10 +0900

    Mathjax related code modified

--- docker/services/ruby/lib/helpers/utilities.rb
+++ docker/services/ruby/lib/helpers/utilities.rb
@@ -8,11 +8,64 @@ module UtilitiesHelper
     CLD.detect_language(text)[:code]
   end
 
-  # Convert markdown to HTML
-  def markdown_to_html(text)
+  def escape_mathjax_chars(text)
+    text.gsub(/\\[a-zA-Z]+/) { |match| "\\" + match }
+  end
+
+  def markdown_to_html(text, mathjax: false)
     text = text.gsub(/\[^([0-9])^\]/) { "[^#{Regexp.last_match(1)}]" }
     text = text.gsub(/(!\[[^\]]*\]\()(['"])([^\s)]+)(['"])(\))/, '\1\3\5')
 
-    Kramdown::Document.new(text, syntax_highlighter: :rouge, input: "GFM", syntax_highlighter_ops: { guess_lang: true }).to_html
+    if mathjax
+      # Arrays to store the mathjax codes
+      block_mathjax = []
+      inline_mathjax = []
+
+      # Replace $$...$$ with placeholders
+      text = text.gsub(/\$\$(.*?)\$\$/m) do
+        block_mathjax << Regexp.last_match(1)
+        "BLOCK_MATHJAX_PLACEHOLDER_#{block_mathjax.size - 1}"
+      end
+
+      block_mathjax.map! do |code|
+        escape_mathjax_chars(code)
+      end
+
+      # Replace $...$ with placeholders
+      text = text.gsub(/\$(.*?)\$/m) do
+        inline_mathjax << Regexp.last_match(1)
+        "INLINE_MATHJAX_PLACEHOLDER_#{inline_mathjax.size - 1}"
+      end
+
+      inline_mathjax.map! do |code|
+        escape_mathjax_chars(code)
+      end
+
+      # Convert markdown to HTML using Kramdown
+      html = Kramdown::Document.new(text,
+                                    syntax_highlighter: :rouge,
+                                    input: "GFM",
+                                    syntax_highlighter_ops: {
+                                      guess_lang: true
+                                    }).to_html
+
+      # Replace placeholders with the original mathjax codes
+      block_mathjax.each_with_index do |code, index|
+        html.gsub!("BLOCK_MATHJAX_PLACEHOLDER_#{index}", "$$#{code}$$")
+      end
+
+      inline_mathjax.each_with_index do |code, index|
+        html.gsub!("INLINE_MATHJAX_PLACEHOLDER_#{index}", "$#{code}$")
+      end
+
+      html
+    else
+      Kramdown::Document.new(text,
+                             syntax_highlighter: :rouge,
+                             input: "GFM",
+                             syntax_highlighter_ops: {
+                               guess_lang: true
+                             }).to_html
+    end
   end
 end

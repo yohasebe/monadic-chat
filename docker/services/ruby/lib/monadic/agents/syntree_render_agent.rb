78c9cbf344e8c5ef1067e625ace582e3860a711b
Yoichiro Hasebe <yohasebe@gmail.com>
2024-08-24 00:34:36 +0900

    syntax tree rendering; mermaid download

--- docker/services/ruby/lib/monadic/agents/syntree_render_agent.rb
+++ docker/services/ruby/lib/monadic/agents/syntree_render_agent.rb
@@ -1,58 +1,26 @@
 require_relative "basic_agent"
 
 module MonadicAgent
-  def syntree_render_agent(text:)
+  def syntree_render_agent(text:, format: "svg", wait: 1)
     return "Error: input text is required." if text.to_s.empty?
 
     tempname = Time.now.to_i.to_s
 
     write_to_file(filename: tempname, extension: "txt", text: text)
 
-    datadir = if IN_CONTAINER
-                File.expand_path(File.join(__dir__, "..", "data"))
-              else
-                File.expand_path(File.join(Dir.home, "monadic", "data"))
-              end
+    sleep wait
 
-    datafile = File.join(datadir, "#{tempname}.txt")
+    success_msg = "Syntree generated successfully"
+    command = "bash -c 'rsyntaxtree -f #{format} -o . #{tempname}.txt'"
+    res = send_command(command: command, container: "ruby", success: success_msg)
 
-    success = false
-    max_retrial = 20
-    max_retrial.times do
-      sleep 1.5
-      if File.exist?(datafile)
-        success = true
-        break
-      end
-    end
-
-    if success
-      command = "bash -c 'rsyntaxtree -o . #{datafile}'"
-      send_command(command: command, container: "ruby", success: "Syntax tree rendered successfully")
-    else
-      return "Error: syntax tree rendering failed."
-    end
-
-    syntree_file = File.join(datadir, "syntree.png")
-
-    success = false
-    max_retrial = 20
-    max_retrial.times do
-      sleep 1.5
-      if File.exist?(syntree_file)
-        success = true
-        break
-      end
-    end
-
-    result_file = File.join(datadir, "#{tempname}.png")
-
-    if success
-      command = "bash -c 'mv #{syntree_file} #{result_file}'"
+    if /\A#{success_msg}/ =~ res.strip
+      sleep wait
+      command = "bash -c 'mv syntree.#{format} #{tempname}.#{format}'"
       send_command(command: command, container: "ruby",
-                   success: "Syntax tree rendered successfully as #{tempname}.png")
+                   success: "Syntax tree generated successfully as #{tempname}.#{format}")
     else
-      "Error: syntax tree rendering failed."
+      "Error: syntax tree generation failed. #{res}"
     end
   end
 end

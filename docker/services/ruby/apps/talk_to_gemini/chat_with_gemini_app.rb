cdcbb437a4212eabacc7278083681f9f9c0a7365
Yoichiro Hasebe <yohasebe@gmail.com>
2024-08-28 08:35:48 +0900

    venders folder introduced

--- docker/services/ruby/apps/talk_to_gemini/chat_with_gemini_app.rb
+++ docker/services/ruby/apps/talk_to_gemini/chat_with_gemini_app.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "./gemini_helper"
-
 class ChatWithGemini < MonadicApp
   include GeminiHelper
 
--- docker/services/ruby/apps/talk_to_mistral/code_with_mistral_app.rb
+++ docker/services/ruby/apps/talk_to_mistral/code_with_mistral_app.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "./mistral_helper"
-
 class CodeWithMistral < MonadicApp
   include MistralHelper
 

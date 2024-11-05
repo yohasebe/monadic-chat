8302e46f3237ab382769ed03a30501a18f3dd59e
Yoichiro Hasebe <yohasebe@gmail.com>
2024-11-06 07:12:28 +0900

    Claude Haiku 3.5 model supported

--- docker/services/ruby/apps/talk_to_claude/chat_with_claude_app.rb
+++ docker/services/ruby/apps/talk_to_claude/chat_with_claude_app.rb
@@ -30,9 +30,8 @@ class ChatWithClaude < MonadicApp
     model: "claude-3-5-sonnet-20241022",
     models: [
       "claude-3-5-sonnet-20241022",
-      "claude-3-5-sonnet-20240620",
+      "claude-3-5-haiku-20241022",
       "claude-3-opus-20240229",
-      "claude-3-haiku-20240307"
     ]
   }
 end

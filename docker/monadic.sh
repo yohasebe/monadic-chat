361479cc4c15e6c49810c405c3219e2dbaf98c41
Yoichiro Hasebe <yohasebe@gmail.com>
2024-11-16 08:48:58 +0900

    initial_prompt_suffix renamed to system_prompt_suffix

--- docker/monadic.sh
+++ docker/monadic.sh
@@ -4,7 +4,7 @@
 export PATH=${PATH}:/usr/local/bin
 
 export SELENIUM_IMAGE="selenium/standalone-chrome:latest"
-export MONADIC_VERSION=0.9.13
+export MONADIC_VERSION=0.9.14
 export HOST_OS=$(uname -s)
 
 RETRY_INTERVAL=5

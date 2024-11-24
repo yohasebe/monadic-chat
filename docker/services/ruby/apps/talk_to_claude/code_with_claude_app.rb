320e1558b212c8a8a4f947ce51dcc89ae0196360
Yoichiro Hasebe <yohasebe@gmail.com>
2024-11-24 17:30:17 +0900

    Jupyter app for Anthropic Claude models reintroduced

--- docker/services/ruby/apps/talk_to_claude/code_with_claude_app.rb
+++ docker/services/ruby/apps/talk_to_claude/code_with_claude_app.rb
@@ -6,7 +6,7 @@ class CodeWithClaude < MonadicApp
   icon = "<i class='fa-solid fa-a'></i>"
 
   description = <<~TEXT
-  This is an application that allows you to run Python code with Anthropic Claude. You can write and execute Python code, install libraries, fetch text from files, and fetch web content. Claude will help you run the code and display the output, including generated images and text data. <a href="https://yohasebe.github.io/monadic-chat/#/language-models?id=anthropic" target="_blank"><i class="fa-solid fa-circle-info"></i></a>.
+  This is an application that allows you to run Python code with Anthropic Claude. You can write and execute Python code, install libraries, fetch text from files, and fetch web content. Claude will help you run the code and display the output, including generated images and text data. <a href="https://yohasebe.github.io/monadic-chat/#/language-models?id=anthropic" target="_blank"><i class="fa-solid fa-circle-info"></i></a>
   TEXT
 
   initial_prompt = <<~TEXT

fe5daee63b05e96cdd2b4d38fcff519ae1df58d4
Yoichiro Hasebe <yohasebe@gmail.com>
2025-02-19 10:46:46 +0900

    Model specific parameter handling improved

--- docker/services/ruby/apps/mail_composer/mail_composer_app.rb
+++ docker/services/ruby/apps/mail_composer/mail_composer_app.rb
@@ -21,7 +21,6 @@ class MailComposer < MonadicApp
     models: OpenAIHelper.list_models,
     model: "gpt-4o-mini",
     temperature: 0.3,
-    top_p: 0.0,
     initial_prompt: initial_prompt,
     easy_submit: false,
     auto_speech: false,

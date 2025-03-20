e9362214ddc42d629326aa8121cb86350f7442cf
Yoichiro Hasebe <yohasebe@gmail.com>
2025-03-20 20:18:22 +0900

    App file restructuring started

--- docker/services/ruby/apps/chat/chat_app.rb
+++ docker/services/ruby/apps/chat/chat_app.rb
@@ -1,34 +1,31 @@
-class Chat < MonadicApp
-  include OpenAIHelper
+class ChatOpenAI < MonadicApp
   include WebSearchAgent
+end
 
-  icon = "<i class='fas fa-comments'></i>"
+class ChatClaude < MonadicApp
+  include WebSearchAgent
+end
 
-  description = <<~TEXT
-  This is the standard application for monadic chat. It can be used in basically the same way as ChatGPT. <a href="https://yohasebe.github.io/monadic-chat/#/basic-apps?id=chat" target="_blank"><i class="fa-solid fa-circle-info"></i></a>
-  TEXT
+class ChatGemini < MonadicApp
+  include WebSearchAgent
+end
 
-  initial_prompt = <<~TEXT
-  You are a friendly and professional consultant with real-time, up-to-date information about almost anything. You are able to answer various types of questions, write computer program code, make decent suggestions, and give helpful advice in response to a prompt from the user. If the prompt is not clear enough, ask the user to rephrase it. Use the same language as the user does. If you are not 100% sure what language it is, keep using English. Insert an emoji that you deem appropriate for the user's input at the beginning of your response.
+class ChatCohere < MonadicApp
+  include WebSearchAgent
+end
 
-    If the response is too long to fit in one message, it can be split into multiple messages. If you need to split in the middle of a code block, be sure to properly enclose the partial code block in each message so that it will display properly as a code block when viewed as HTML.
-  TEXT
+class ChatMistral < MonadicApp
+  include WebSearchAgent
+end
 
-  @settings = {
-    group: "OpenAI",
-    disabled: !CONFIG["OPENAI_API_KEY"],
-    models: OpenAIHelper.list_models,
-    model: "gpt-4o",
-    context_size: 100,
-    initial_prompt: initial_prompt,
-    description: description,
-    easy_submit: false,
-    auto_speech: false,
-    app_name: "Chat",
-    icon: icon,
-    initiate_from_assistant: false,
-    image: true,
-    pdf: false,
-    websearch: true
-  }
+class ChatGrok < MonadicApp
+  include WebSearchAgent
+end
+
+class ChatDeepSeek < MonadicApp
+  include WebSearchAgent
+end
+
+class ChatPerplexity < MonadicApp
+  include WebSearchAgent
 end

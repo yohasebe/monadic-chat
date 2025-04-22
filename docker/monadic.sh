cd88582b19e762c51ce5aa8e3fc1594b0c6598ab
Yoichiro Hasebe <yohasebe@gmail.com>
2025-04-22 20:49:26 +0900

    Check-for-update related issues addressed

--- docker/monadic.sh
+++ docker/monadic.sh
@@ -3,7 +3,7 @@
 # Add /usr/local/bin to the PATH
 export PATH=${PATH}:/usr/local/bin
 
-export MONADIC_VERSION=0.9.86
+export MONADIC_VERSION=0.9.87
 export HOST_OS=$(uname -s)
 
 RETRY_INTERVAL=5
--- docker/services/ruby/lib/monadic/version.rb
+++ docker/services/ruby/lib/monadic/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Monadic
-  VERSION = "0.9.86"
+  VERSION = "0.9.87"
 end

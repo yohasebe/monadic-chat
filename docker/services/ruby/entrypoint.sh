94118e708a664c65953c3572b83fe05f67d88df7
Yoichiro Hasebe <yohasebe@gmail.com>
2025-05-04 08:08:59 +0900

    Several small improvements

--- docker/services/ruby/entrypoint.sh
+++ docker/services/ruby/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Attempt to start the thin server and capture any errors
-thin start -R config.ru -p 4567 -e development > /monadic/log/server.log 2>&1
+# Prepare log directory
+mkdir -p /monadic/log
+
+# Run Thin server with optimized settings for faster startup
+thin start -R config.ru -p 4567 -e development -d -l /monadic/log/server.log
 
 # Check if the thin server started successfully
 if [ $? -ne 0 ]; then

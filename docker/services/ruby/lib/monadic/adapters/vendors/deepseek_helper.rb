38f418c4fa062044d1638444f48ae2e88d6b7a9f
Yoichiro Hasebe <yohasebe@gmail.com>
2026-06-11 00:20:03 +0900

    Use shared retry POST in deepseek_helper

--- docker/services/ruby/lib/monadic/adapters/vendors/deepseek_helper.rb
+++ docker/services/ruby/lib/monadic/adapters/vendors/deepseek_helper.rb
@@ -121,23 +121,9 @@ module DeepSeekHelper
     
     http = HTTP.headers(headers)
     
-    # Simple retry logic
-    response = nil
-    MAX_RETRIES.times do
-      begin
-        response = http.timeout(
-          connect: open_timeout,
-          write: write_timeout,
-          read: read_timeout
-        ).post(target_uri, json: body)
-        
-        break if response && response.status && response.status.success?
-      rescue HTTP::Error, HTTP::TimeoutError
-        # Continue to next retry
-      end
-      
-      sleep RETRY_DELAY
-    end
+    response = post_json_with_retries(http, target_uri, body,
+                                      max_retries: MAX_RETRIES, retry_delay: RETRY_DELAY,
+                                      rescue_errors: [HTTP::Error, HTTP::TimeoutError])
     
     # Process response
     if response && response.status && response.status.success?
@@ -1084,15 +1070,8 @@ module DeepSeekHelper
       body.delete("tool_choice")
     end
 
-    res = nil
-    MAX_RETRIES.times do
-      res = http.timeout(connect: open_timeout,
-                         write: write_timeout,
-                         read: read_timeout).post(target_uri, json: body)
-      break if res.status.success?
-
-      sleep RETRY_DELAY
-    end
+    res = post_json_with_retries(http, target_uri, body,
+                                 max_retries: MAX_RETRIES, retry_delay: RETRY_DELAY)
 
     unless res.status.success?
       error_report = JSON.parse(res.body)
